e22fc8f9194f75c0fd6370fba44196d5f780ef74
hongmeiliu <hongmeiliu@deeproute.ai>
2025-02-26 19:01:37 +0800

    run_coroutine_threadsafe的用法

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ async def my_coroutine():
 def run_in_thread(loop, event):
     asyncio.set_event_loop(loop)
     loop.run_forever()
-    event.set()  # 通知主线程协程已完成
+    event.set()  # 通知主线程事件循环已停止
 
 
 async def main():
@@ -22,14 +22,15 @@ async def main():
     thread.start()
 
     # 确保事件循环已经启动
-    while not loop.is_running():
-        await asyncio.sleep(0.1)
+    # while not loop.is_running():
+    #     await asyncio.sleep(0.1)
 
-    future = asyncio.run_coroutine_threadsafe(my_coroutine(), loop)
+    future = asyncio.run_coroutine_threadsafe(my_coroutine(), loop)  # 跨线程投递协程
     result = future.result()  # 等待协程完成并获取结果
     print(f"Result: {result}")
 
     loop.call_soon_threadsafe(loop.stop)
+    event.wait()  # 等待子线程通知事件循环已停止
     thread.join()  # 确保子线程已经完成
     loop.close()
 
