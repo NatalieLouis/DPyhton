181f3272c8f748c61a4d93c81b693cc919d5955b
hongmeiliu <hongmeiliu@deeproute.ai>
2025-02-26 11:13:01 +0800

    bugs

--- main.py
+++ main.py
@@ -1,24 +1,16 @@
-import logging
+import asyncio
+import time
 
-# 配置日志
-logging.basicConfig(
-    level=logging.INFO,  # 设置日志级别为 INFO
-    format="%(asctime)s %(levelname)s: %(message)s",  # 日志格式
-    datefmt="%H:%M:%S,%f",  # 时间戳的格式
-)
 
-# 创建一个文件处理器，将日志输出到文件
-file_handler = logging.FileHandler("/tmp/park.log", mode='w')
-file_handler.setLevel(logging.INFO)  # 设置文件处理器的日志级别
-file_handler.setFormatter(logging.Formatter("%(asctime)s %(levelname)s: %(message)s", datefmt="%H:%M:%S,%f"))
+def blocking_function(proto):
+    print(f"Blocking function started with {proto}")
+    time.sleep(2)  # 模拟阻塞操作
+    print(f"Blocking function finished with {proto}")
 
-# 获取根日志记录器
-logger = logging.getLogger()
 
-# 将文件处理器添加到根日志记录器
-logger.addHandler(file_handler)
+async def main():
+    proto = "example_proto"
+    loop = asyncio.get_running_loop()
+    loop.run_in_executor(None, blocking_function, proto)  # 在默认线程池中运行阻塞函数
 
-# 测试日志
-logger.info("This is an info message3")
-logger.warning("This is a warning message3")
-logger.error("This is an error message")
+asyncio.run(main())
