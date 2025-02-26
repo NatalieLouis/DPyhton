3f72f7cc61c76e9e8e341724201b8051c06b0b6f
hongmeiliu <hongmeiliu@deeproute.ai>
2025-02-26 21:31:36 +0800

    主线程获取子线程的future结果

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import asyncio
+import threading
 
 
-async def async_task(future):
+async def async_task():
     await asyncio.sleep(1)
-    future.set_result("Task Completed!")
+    return "Result from async_task"
+
+
+def thread_func(loop, future):
+    print(f"Thread running in: {threading.current_thread().name}")
+    asyncio.run_coroutine_threadsafe(async_task(), loop).add_done_callback(
+        lambda f: future.set_result(f.result())
+    )
 
 
 async def main():
+    loop = asyncio.get_running_loop()
     future = asyncio.Future()
-    asyncio.create_task(async_task(future))
-    result = await future  # ✅ 异步等待结果
-    print(f"Future result: {result}")
+    thread = threading.Thread(target=thread_func, args=(loop, future))
+    thread.start()
+    result = await future  # ✅ 异步等待线程中任务结果
+    print(f"Got result: {result}")
+    thread.join()
 
 asyncio.run(main())
