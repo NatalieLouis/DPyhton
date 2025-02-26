61de3ce3b0b070efc7852a994b764c5c042877be
hongmeiliu <hongmeiliu@deeproute.ai>
2025-02-26 11:45:34 +0800

    fix bug

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import asyncio
 import time
+from concurrent.futures import ThreadPoolExecutor
 
 
 def blocking_function(proto):
     print(f"Blocking function started with {proto}")
     time.sleep(2)  # 模拟阻塞操作
-    print(f"Blocking function finished with {proto}")
+    
 
 
 async def main():
     proto = "example_proto"
     loop = asyncio.get_running_loop()
-    loop.run_in_executor(None, blocking_function, proto)  # 在默认线程池中运行阻塞函数
+    with ThreadPoolExecutor() as executor:
+        await loop.run_in_executor(executor, blocking_function, proto)  # 在默认线程池中运行阻塞函数
 
 asyncio.run(main())
