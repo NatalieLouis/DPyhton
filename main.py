8860094105e63f69736cc7b286a4abc7c9df6ea1
hongmeiliu <hongmeiliu@deeproute.ai>
2025-02-26 22:13:53 +0800

    用同步包装异步

--- main.py
+++ main.py
@@ -2,13 +2,20 @@ import asyncio
 import threading
 
 
-async def async_callback():
-    print(f"Async callback executed in {threading.current_thread().name}")
+async def async_task():
+    print(f"Async task executed in {threading.current_thread().name}")
+    await asyncio.sleep(1)
+    print("Async task finished")
+
+
+def sync_callback(loop):
+    # 通过调用同步函数来启动异步任务
+    asyncio.ensure_future(async_task())
 
 
 def thread_func(loop):
     print(f"Thread running in: {threading.current_thread().name}")
-    loop.call_soon_threadsafe(async_callback)  # 会报错
+    loop.call_soon_threadsafe(sync_callback, loop)
 
 
 async def main():
