d547efcbf601329aa9429f47cfc1d9dacb6ae23e
hongmeiliu <hongmeiliu@deeproute.ai>
2025-04-18 17:16:50 +0800

    gater 与Task

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import asyncio
-async def heartbeat():
-    while True:
-        print("ping")
-        await asyncio.sleep(5)
-
-async def business():
-    for i in range(3):
-        print("doing business...")
-        await asyncio.sleep(2)
+async def slow_op():
+    await asyncio.sleep(3)
+    return "done"
+    
+async def do_work(n):
+    await asyncio.sleep(n)
+    return f"done: {n}"
 
 async def main():
-    asyncio.create_task(heartbeat())
-    await business()
+    # 虽然传的是协程对象，但 gather 会帮你转成 Task 并执行
+    result = await asyncio.gather(slow_op(), slow_op())
+    print(result)
+
+async def do_work(n):
+    await asyncio.sleep(n)
+    return f"done: {n}"
+
+async def main2():
+    tasks = [asyncio.create_task(do_work(i)) for i in range(5)]
+    results = await asyncio.gather(*tasks)
+    print(results)
 
 asyncio.run(main())
