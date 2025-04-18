31e774c3acbdc5a524894b38751117a6d38a34df
hongmeiliu <hongmeiliu@deeproute.ai>
2025-04-18 17:27:35 +0800

    等待Task超时

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import asyncio
 
-async def bad_task():
-    await asyncio.sleep(1)
-    raise ValueError("Boom!")
+async def slow_func():
+    await asyncio.sleep(5)
+    return "done"
 
 async def main():
-    # 创建了一个任务，但没 await 也没 try-except
-    asyncio.create_task(bad_task())
-    await asyncio.sleep(2)
-    print("Main done")
+    task = asyncio.create_task(slow_func())  # 创建了 Task 对象
+
+    try:
+        result = await asyncio.wait_for(task, timeout=2)  # 等待 Task 的完成
+        print("Result:", result)
+    except asyncio.TimeoutError:
+        print("Task timed out!")
+        print("Cancelling task...")
+        task.cancel()  # 手动取消任务
+        try:
+            await task  # 等待取消完成（很重要）
+        except asyncio.CancelledError:
+            print("Task was cancelled.")
 
 asyncio.run(main())
